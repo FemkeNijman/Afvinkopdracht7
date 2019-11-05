2ad5e111f951e83815324e70c4e79427bee6fd20
FemkeNijman <55234333+FemkeNijman@users.noreply.github.com>
2019-11-05 16:50:06 +0100

    Add files via upload

--- opdracht2.py
+++ opdracht2.py
@@ -1,46 +1,68 @@
 def lees_inhoud(bestandnaam):
-    """ input: bestandnaam
+    """opent het bestand en zet het in een list. hierna worden [i][0] en [i][1] gekozen en in een 2d lijst gezet.
 
-output: kolomAB
-"""
+    input:
+    bestandnaam - string
+    output:
+    return kolomAB - list
+    """
     #zet het bestand in een list
-    bestand = open(bestandnaam)
-    lijst1 = []
-    for regel in bestand:
-        regel = regel.rstrip().split(',')
-        lijst1.append(regel)
+    try:
+        bestand = open(bestandnaam)
+        lijst1 = []
+        for regel in bestand:
+            regel = regel.rstrip().split(',')
+            lijst1.append(regel)
 
     #kiest alleen [i][0] en i[1] en zet deze in een 2d lijst
-    kolomAB = []
-    for i in range(1,len(lijst1)):
-        kolomAB.append([lijst1[i][0], float(lijst1[i][1])])
-    print(kolomAB[0:10])
-    return kolomAB
+        kolomAB = []
+        for i in range(1,len(lijst1)):
+            kolomAB.append([lijst1[i][0], float(lijst1[i][1])])
+        print(kolomAB[0:10])
+        return kolomAB
+
+    except IOError:
+        print("Dit bestand kan niet gevonden worden")
+    except ValueError:
+        print("Conversie van getal mislukt")
 
 def gemiddeldeberekenen(kolomAB):
-    """
-input: kolomAB
-output: totaal aantal en gemiddelde
+    """rekent het totaalaantal en het gemiddelde uit
 
-"""
-    totaalaantal = 0
-    for i in range(len(kolomAB)):
-        totaalaantal += kolomAB[i][1]
-    gemiddelde = totaalaantal/len(kolomAB)
-    print(totaalaantal)
-    print(gemiddelde)
+    input:
+    kolomAB - list
+    output:
+    totaalaantal, gemiddelde - float
+    """
+
+    try:
+        totaalaantal = 0
+        for i in range(len(kolomAB)):
+            totaalaantal += kolomAB[i][1]
+        gemiddelde = totaalaantal/len(kolomAB)
+        print(totaalaantal)
+        print(gemiddelde)
+
+    except ValueError:
+        print("Conversie van getal mislukt")
 
 def groterdan50(kolomAB):
-    """
-input: kolomAB
-output: groterlijst
+    """Kijkt welke stukken in kolomAB groter zijn dan 50 en zet ze in een nieuwe lijst
 
-"""
-    groterlijst = []
-    for i in range(len(kolomAB)):
-        if kolomAB[i][1] >= 50:
-            groterlijst.append(kolomAB[i][0])
-    return groterlijst
+    input:
+    kolomAB - list
+    output:
+    return groterlijst - list
+    """
+    try:
+        groterlijst = []
+        for i in range(len(kolomAB)):
+            if kolomAB[i][1] >= 50:
+                groterlijst.append(kolomAB[i][0])
+        return groterlijst
+    
+    except ValueError:
+        print("Conversie van getal mislukt")
     
 
 def main():
